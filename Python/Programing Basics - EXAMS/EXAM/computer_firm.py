1614d39c24d917cf39acc33a467567abf0421e20
GeorgeIT <123578800+georgi-blue@users.noreply.github.com>
2024-03-11 16:57:24 +0200

    Added comments on computer_firm.py

--- Python/Programing Basics - EXAMS/EXAM/computer_firm.py
+++ Python/Programing Basics - EXAMS/EXAM/computer_firm.py
@@ -1,18 +1,18 @@
-n_computers = int(input())
+n_computers = int(input())  ## user input (whole) number.
 
 total_maked_sales = 0
 total_rating_counter = 0
 rating_sum = 0
 
-for _ in range(n_computers):
-    computer_model = input()
+for _ in range(n_computers): ## every iteration is a new computer
+    computer_model = input() ## new computer
     total_sales = 0
 
-    rating = int(computer_model[2])
-    sales = int(computer_model[:2])
+    rating = int(computer_model[2]) ## getting index (2)
+    sales = int(computer_model[:2]) ## getting sliced index to index 2 - (0,1)
 
     if rating == 2:
-        total_sales = sales * 0
+        total_sales = sales * 0  
     elif rating == 3:
         total_sales = sales * 0.5
     elif rating == 4:
@@ -22,9 +22,9 @@ for _ in range(n_computers):
     elif rating == 6:
         total_sales = sales
 
-    total_maked_sales += total_sales
-    total_rating_counter += 1
-    rating_sum += rating
+    total_maked_sales += total_sales    ## adding the current sales to all sales
+    total_rating_counter += 1     ## adding the rating
+    rating_sum += rating      ## sum the rating
 
 print(f"{total_maked_sales:.2f}")
 print(f"{rating_sum / total_rating_counter:.2f}")
