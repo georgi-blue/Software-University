8e725cb62e6dec749d8192b9c35f6997b7df2f52
G-Tech <george.tech251@gmail.com>
2024-03-12 17:13:56 +0200

    Added more solutions

---
+++ Python/OOP/Static and Class Methods/Hotel Rooms/project/room.py
@@ -0,0 +1,22 @@
+class Room:
+    def __init__(self, number: int, capacity: int):
+        self.number = number
+        self.capacity = capacity
+        self.guests = 0
+        self.is_taken = False
+
+    def take_room(self, people: int) -> str or None:
+        if not self.is_taken and people <= self.capacity:
+            self.guests = people
+            self.is_taken = True
+            return
+
+        return f"Room number {self.number} cannot be taken"
+
+    def free_room(self) -> str or None:
+        if self.is_taken:
+            self.is_taken = False
+            self.guests = 0
+            return
+
+        return f"Room number {self.number} is not taken"
